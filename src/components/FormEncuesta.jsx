17cd362aa44dd2f1cd60fc84c996866777623283
Hector Dario SOL <hectordsol@gmail.com>
2023-01-08 12:35:28 -0300

    fix form

--- src/components/FormEncuesta.jsx
+++ src/components/FormEncuesta.jsx
@@ -3,18 +3,18 @@ const FormEncuesta = () =>{
 
     return (
         <form className="card card-body">
-            <div className="form-group"></div>
-                <label for="full_name" class="form-label mt-4">Nombre Completo</label>
+            <div className="form-group input-group"></div>
+                <label className="form-label mt-4">Nombre Completo</label>
                 <input type="text" className="form-control" placeholder="Nombre completo"
                 id="full_name"/>
-                <label for="email" class="form-label mt-4">Correo Electrónico</label>
+                <label className="form-label mt-4">Correo Electrónico</label>
                 <input type="email" className="form-control" placeholder="Correo electrónico"
                 id="email"/>
-                <label for="birth_date" class="form-label mt-4">Fecha de Nacimiento</label>
+                <label className="form-label mt-4">Fecha de Nacimiento</label>
                 <input type="date" className="form-control" placeholder="Fecha de Nacimiento"
                 id="birth_date"/>
-                <label for="country_of_origin" class="form-label mt-4">¿Cuál es tu país de origen?</label>
-                <select class="form-select" id="country_of_origin">
+                <label className="form-label mt-4">¿Cuál es tu país de origen?</label>
+                <select className="form-control form-select" id="country_of_origin">
                     <option value="argentina">Argentina</option>
                     <option value="brasil">Brasil</option>
                     <option value="chile">Chile</option>
@@ -24,11 +24,11 @@ const FormEncuesta = () =>{
                     <option value="uruguay">Uruguay</option>
                     <option value="venezuela">Venezuela</option>
                 </select>
-
-<div class="form-check">
-        <input className="form-control" class="form-label mt-4" type="checkbox" value="" id="terms_and_conditions"/>
-        <label for="terms_and_conditions" class="form-label mt-4">¿Acepta los términos y condiciones?</label>
-      </div>
+                <div className="form-control form-check mt-4">
+                    <input className="form-label mt-4" type="checkbox" value="" 
+                    id="terms_and_conditions"/>
+                    <label className="form-label mt-4">¿Acepta los términos y condiciones?</label>
+                </div>
         </form>
     )
 };
